f58595fbfe07e2e79dfafbb0bba3821e474d1143
shukurillaevumar <97353393+shukurillaevumar@users.noreply.github.com>
2023-02-02 18:22:28 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Form from './Components/form';
+import Header from './Components/header';
+import Main from './Components/main';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
